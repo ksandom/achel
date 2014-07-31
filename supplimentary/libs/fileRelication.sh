c9407350d287de167e1e48d8c8324bb81d68ace4
Kevin Sandom <kevinsandom@yahoo.co.uk>
2014-07-31 09:27:29 +0100

    File replication: Work on managing individual files.

--- supplimentary/libs/fileRelication.sh
+++ supplimentary/libs/fileRelication.sh
@@ -53,6 +53,13 @@ function setDefaultProvider
 	fi
 }
 
+function getProviderPath
+{
+	local providerName="$1"
+	
+	collectionGetValue "FileReplication" "Providers,$providerName"
+}
+
 function getDefaultProvider
 {
 	local providerName="$1"
@@ -63,6 +70,55 @@ function getDefaultProvider
 function autoSetupReplicators
 {
 	addProvider "dropbox" ~/Dropbox 2>/dev/null
-	addProvider "googleDrive" ~/gDrive 2>/dev/null
-	addProvider "dropbox-test" ~/Dropbox 2>/dev/null
+	
+	# TODO Add more providers
+}
+
+function fileRepListFiles
+{
+	local fileRegex="$1"
+	if [ "$fileRegex" != "" ]; then
+		fileRepListFilesInAllFolders | grep "$fileRegex"
+	else
+		fileRepListFilesInAllFolders
+	fi
+}
+
+function fileRepListFilesInAllFolders
+{
+	fileRepListFilesIn data
+}
+
+function fileRepListFilesIn
+{
+	local folderName="$1"
+	while read file	destination;do
+		echo "$folderName/$file    points to     $destination"
+	done < <(resolveSymlinks "$configDir/$folderName")
+}
+
+function fileRepAddFile
+{
+	local fileToAdd="$1"
+	local provider="$2"
+	
+	# Use the default provider if we don't have one.
+	if [ "$provider" == "" ]; then
+		provider=`getDefaultProvider`
+	fi
+	
+	# TODO add check to make sure that the selected provider (via either method), actually exists
+	
+	local providerPath=`getProviderPath "$provider"`
+	# Check that the path of the selected provider exists
+	if [ ! -e "$providerPath" ]; then
+		# TODO write error
+		echo "fileRepAddFile: "
+		return 1
+	fi
+	
+	cd "$configDir"
+	# TODO write this
+	
+	cd ~-
 }
