cb6d3c148d6a014245b17d8cf04e477ae692e738
Kevin Sandom <kevinsandom@yahoo.co.uk>
2015-03-19 19:42:06 +0000

    PHPEnv: Added PHPEnv which provides environment variables.

---
+++ packages-available/PHPEnv/docs/readme.md
@@ -0,0 +1,5 @@
+# PHPEnv - Environment variables from PHP
+
+This package gives access to the environment variables that PHP provides. These are just lazy copied and may not all be re-implemented if Achel is re-implemented in another language.
+
+
---
+++ packages-available/PHPEnv/phpEnv.php
@@ -0,0 +1,46 @@
+<?php
+# Copyright (c) 2015, Kevin Sandom under the BSD License. See LICENSE for full details.
+
+class PHPEnv extends Module
+{
+	function __construct()
+	{
+		parent::__construct(__CLASS__);
+	}
+	
+	function event($event)
+	{
+		switch ($event)
+		{
+			case 'init':
+				$this->core->registerFeature($this, array('getPHPEnv'), 'getPHPEnv', 'Get the PHP environment variables. --getEnv=Category,variable', array('env','dev'));
+				$this->core->registerFeature($this, array('getEnv', 'getPHPServer'), 'getPHPServer', 'Get the PHP server variables. --getSystem. --getServer=Category,variable', array('env','dev'));
+				$this->core->registerFeature($this, array('getPHPRequest'), 'getPHPRequest', 'Get the PHP request variables. For Achel, this is very much an edge case. You probably want one of the other Env variables such as --getEnv --getSystem. --getRequest=Category,variable', array('env','dev'));
+				break;
+			case 'followup':
+				break;
+			case 'last':
+				break;
+			case 'getPHPEnv':
+				$parms=$this->core->interpretParms($this->core->get('Global', $event),2 ,2);
+				$this->core->set($parms[0], $parms[1], $_ENV);
+				break;
+			case 'getPHPServer':
+				$parms=$this->core->interpretParms($this->core->get('Global', $event),2 ,2);
+				$this->core->set($parms[0], $parms[1], $_SERVER);
+				break;
+			case 'getPHPRequest':
+				$parms=$this->core->interpretParms($this->core->get('Global', $event),2 ,2);
+				$this->core->set($parms[0], $parms[1], $_REQUEST);
+				break;
+			default:
+				$this->core->complain($this, 'Unknown event', $event);
+				break;
+		}
+	}
+}
+
+$core=core::assert();
+$core->registerModule(new PHPEnv());
+ 
+?>
