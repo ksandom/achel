f8734fa05f3c81ece037e7b4145cec6e117cf56f
Kevin Sandom <kevinsandom@yahoo.co.uk>
2016-04-29 07:49:38 +0100

    Logic: Added the ability to append an extra channel to the atLeast output.

--- packages-available/AchelLogic/achelLogic.php
+++ packages-available/AchelLogic/achelLogic.php
@@ -21,6 +21,7 @@ class LogicFaucets extends Faucets
 		{
 			case 'init':
 				$this->core->registerFeature($this, array('createAtLeastFaucet'), 'createAtLeastFaucet', "Create a faucet that will only send anything if if there are at least N channels that have pending information. At that point it sends everything. Previous iterations where the requirement was not met, will be discarded. --createAtLeastFaucet=faucetName,N", array());
+				$this->core->registerFeature($this, array('createAtLeastAndAddFaucet'), 'createAtLeastAndAddFaucet', "Create a faucet that will only send anything if if there are at least N channels that have pending information. At that point it sends everything and sends a preset value to a preset channel. Previous iterations where the requirement was not met, will be discarded. --createAtLeastAndAddFaucet=faucetName,N,channel,value", array());
 				
 				break;
 			case 'followup':
@@ -30,6 +31,10 @@ class LogicFaucets extends Faucets
 			case 'createAtLeastFaucet':
 				$parms=$this->core->interpretParms($this->core->get('Global', $event), 2, 2);
 				$this->environment->currentFaucet->createFaucet($parms[0], 'regex', new AtLeast($parms[1]));
+				break;
+			case 'createAtLeastAndAddFaucet':
+				$parms=$this->core->interpretParms($this->core->get('Global', $event), 4, 4);
+				$this->environment->currentFaucet->createFaucet($parms[0], 'regex', new AtLeast($parms[1], $parms[2], $parms[3]));
 				break;
 			
 			default:
@@ -42,18 +47,32 @@ class LogicFaucets extends Faucets
 class AtLeast extends ThroughBasedFaucet
 {
 	private $numberOfRequiredChannels=0;
+	private $channel=false;
+	private $value='';
 	
-	function __construct($numberOfRequiredChannels)
+	function __construct($numberOfRequiredChannels, $channel=false, $value=false)
 	{
 		parent::__construct(__CLASS__);
 		
 		$this->numberOfRequiredChannels=$numberOfRequiredChannels;
+		
+		if ($channel)
+		{
+			$this->channel=$channel;
+			$this->value=$value;
+		}
 	}
 	
 	function preGet()
 	{
 		$gotSomething=false;
 		$channelCount=count($this->input);
+		
+		if ($channelCount>=$this->numberOfRequiredChannels and $this->channel)
+		{
+			$this->outFill(array($this->value), $this->channel);
+			$gotSomething=true;
+		}
 		
 		foreach ($this->input as $channel=>$data)
 		{
