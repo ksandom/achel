af0d2727661de86db85890eb2465ade70b57112f
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-03-13 08:03:29 +0000

    Bug: Added "manageAchel bugReportCreate"

--- supplimentary/libs/bug.sh
+++ supplimentary/libs/bug.sh
@@ -2,8 +2,24 @@
 
 function creatBugReport
 {
-	profileName="$1"
+	commandName="$1"
 	
-	echo "Cache stats"
-	achel --cacheStats
+	echo;echo "Cache stats"
+	$commandName --cacheStats
+	
+	echo;echo "Settings"
+	$commandName --getCategory=Settings
+	
+	echo;echo "General"
+	$commandName --getCategory=General
+	
+	possibleRepoPath=`$commandName --set='Local,possibleRepoPath,~!General,configDir!~/repos/~!General,programName!~' --get=Local,possibleRepoPath -s`
+	
+	echo;echo "Recent git items"
+	if [ -e "$possibleRepoPath" ]; then
+		cd "$possibleRepoPath"
+		git log -n 5 --pretty=oneline
+	else
+		echo "Was not able to reliably determine the repo path."
+	fi
 }
